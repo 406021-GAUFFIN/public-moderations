fc1f4e0572a59aae65b312b75545785d9680e9ae
lgaufin <lucas@magnetico.dev>
2024-09-24 18:44:46 -0300

    wip

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,6 @@
 INSERT INTO sanction_type (id, created_by, created_date, last_updated_at , last_updated_by , description, name, sanction_severity) VALUES
 (999, 1, '2024-06-12 10:00:00', '2024-06-12 10:00:00', 1, 'Ruidos molestos', 'Ruidos molesos', 'HIGH');
 
-
-
 INSERT INTO fine (id , created_by, created_date , last_updated_at , last_updated_by,  fine_state ,  plot_id , sanction_type )VALUES
     (999, 1, '2024-06-12 10:00:00', '2024-06-12 10:00:00', 1, 'ON_ASSEMBLY',  1, 999);
 INSERT INTO fine (id , created_by, created_date , last_updated_at , last_updated_by,  fine_state ,plot_id , sanction_type )VALUES
@@ -21,6 +19,5 @@ INSERT INTO fine (id , created_by, created_date , last_updated_at , last_updated
     (1006, 1, '2024-06-12 10:00:00', '2024-06-12 10:00:00', 1,  'ON_ASSEMBLY',  1, 999);
 INSERT INTO fine (id , created_by, created_date , last_updated_at , last_updated_by, fine_state ,plot_id , sanction_type )VALUES
     (1007, 1, '2024-06-12 10:00:00', '2024-06-12 10:00:00', 1,  'ON_ASSEMBLY',  1, 999);
-
 insert into  infraction (id , created_by , created_date , last_updated_at , last_updated_by, description , infraction_state  , plot_id, fine_id , sanction_type )
     VALUES (1005, 1, '2024-06-12 10:00:00', '2024-06-12 10:00:00', 1, 'Molestaba demasiado ', 'APPROVED',1, 999, 999)

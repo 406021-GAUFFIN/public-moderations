be8785dea608aae28d8857223318f08813c815d4
Alejandro Ziade <109633897+405141-Ziade-Alejandro@users.noreply.github.com>
2024-10-20 00:43:43 -0300

    just checkign the tests

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/CreateInfractionDto.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/CreateInfractionDto.java
@@ -7,6 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+
+/**
+ * DTO for the creation of the infraction.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
